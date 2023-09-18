447c3b81d33bf9c716610e6034c07193d50e5aae
懋和道人 <15062733214@qq.com>
2023-09-19 04:42:01 +0800

    Update foot.vue

--- src/.vuepress/components/foot.vue
+++ src/.vuepress/components/foot.vue
@@ -3,7 +3,7 @@
     <p class="beian"><a href="https://beian.miit.gov.cn" target="_blank">苏ICP备2021008246号-20</a>、<a target="_blank" rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32061202001196">苏公网安备32061202001196号</a></p>
     <p>© <a href="https://www.wulintang.cn/" target="_blank">南通伍林堂文化传播有限公司</a> 版权所有</p>
     <p>本网站由 <a href="http://www.ccwadj.cn/" target="_blank">云之道数据</a> 提供云计算服务</p>    
-    <p><a target="_blank" rel="nofollow" href="https://www.wulintang.xin/new/202107081.shtml"><img border="0" src="https://cdn.jsdelivr.net/gh/wulintang/cdn/yun/yun.png" style="height:38px!important;vertical-align: middle;"></a></p>
+    <p><a target="_blank" rel="nofollow" href="https://www.wulintang.xin/new/202107081.shtml"><img border="0" src="https://cdn.wulintang.cn/cdn/yun/yun.png" style="height:38px!important;vertical-align: middle;"></a></p>
   <p><a target="_blank" title="51la网站统计" href="https://v6.51.la/land/JfZdzouXersbJonA">51LA统计</a></p>
   </div>
 </template>
